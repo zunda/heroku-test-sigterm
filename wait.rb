bcc0ac1352d4c0565b1ba3c3435910ce1dc9ca9a
zunda <zundan@gmail.com>
2020-10-05 16:39:52 -1000

    Initial commit for code

---
+++ wait.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+
+puts "Starting parent: #{Process.pid}"
+Signal.trap("TERM") do
+  puts "Parent #{Process.pid} got a SIGTERM. Exiting in 10 seconds."
+  sleep 10
+  exit 0
+end
+
+children = []
+0.upto(5) do |w|
+  children << Process.fork do
+    Signal.trap("TERM") do
+      puts "Child #{Process.pid} got a SIGTERM. Ignoring."
+    end
+    puts "Starting child: #{Process.pid}"
+    sleep w + 60
+    exit 0
+  end
+end
+
+children.each do |child|
+  pid, status = Process.waitpid2(child)
+  puts "Child #{pid} exited with status #{status}"
+end
+
+sleep 30*3600
